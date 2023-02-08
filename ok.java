569caaeb90199c7b039a73dc4b703a252bdbc1fe
Khan <khanzaid0816@gmail.com>
2023-02-08 13:36:23 +0530

    version 2

--- ok.java
+++ ok.java
@@ -1,5 +1,10 @@
 class ok{
     public static void main(String[] args) {
        System.out.println("haha"); 
+
     }
+    public void method(int value){
+       System.out.print(value);
+      }
+
 }
